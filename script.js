ff417e1ef221e8faa34a1c4645883b4d46dc292a
ko9612 <kohana735@gmail.com>
2022-07-31 23:41:50 +0900

    css 수정 + 페이징, 로컬스토리지 저장 기능 추가

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
+let data;
+const dataFromLocalStorage = localStorage.getItem('agoraStatesDiscussions')
+if (dataFromLocalStorage) {
+  data = JSON.parse(dataFromLocalStorage)
+} else {
+  data = agoraStatesDiscussions.slice()
+}
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
-  
   // 함수의 목적: li요소 안에 있는 하위요소들을 새로 만듬
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
@@ -22,40 +28,97 @@ const convertToDiscussion = (obj) => {
 
   const discussionTitle = document.createElement("h2");
   const TitleHref = document.createElement("a");
-  discussionContent.append(TitleHref);
   TitleHref.textContent = obj.title;
   TitleHref.href = obj.url;
+  discussionContent.append(TitleHref);
 
   const discussionInfo = document.createElement("div");
-  discussionInfo.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString()}`; //현지 시간에 맞게 표현 (ex. 오전 10:02:17)
+  discussionInfo.className = "discussion__information";
+  discussionInfo.textContent = `${obj.author} / ${new Date(
+    obj.createdAt
+    ).toLocaleString()}`; //현지 시간에 맞게 표현 (ex. 오전 10:02:17)
   discussionContent.append(discussionTitle, discussionInfo);
 
   const check = document.createElement("p");
-
-  obj.answer !== null ? check.textContent = "☑" : check.textContent = "☒";
-  // if(obj.answer !== null){
-  //   check.textContent = "☑";
-  // }else{
-  //   check.textContent = "☒";
-  // }
+  obj.answer !== null ? check.textContent = "✔" : check.textContent = "✘";
   discussionAnswered.append(check);
-
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
 
-// 디스커션 추가 기능 구현, `section.form__container` 요소에 새로운 아고라 스테이츠 질문을 추가할 수 있는 입력 폼을 제작
-//`agoraStatesDiscussions` 배열에 추가한 데이터가 실제 쌓여야 합니다.
+// data 배열의 모든 데이터를 화면에 렌더링하는 함수.
+const render = (element, from, to) => {
+  console.log(from, to)
+  if(!from && !to){
+    from = 0; to = data.length-1;
+  }
+  // 다 지우고 배열에 있는 내용 다 보여주기
+  while(element.firstChild){
+    element.removeChild(element.firstChild);
+  }
+  for(let i = from; i < to; i++){
+    element.append(convertToDiscussion(data[i]));
+  }
+   return;
+};
 
-//form 선택
+  // 페이지네이션을 위한 변수
+  let limit = 10; page = 1;
+
+  // ul 요소에 data 배열의 모든 데이터를 화면에 렌더링한다.
+  const ul = document.querySelector("ul.discussions__container");
+  render(ul, 0, limit);
+
+  const getPageStartEnd = (limit, page) => {
+    const len = data.length - 1
+    let pageStart = Number(page-1) * Number(limit);
+    let pageEnd = Number(pageStart) + Number(limit);
+    if(page <= 0){
+      pageStart = 0;
+    }
+    if(pageEnd >= len){
+      pageEnd = len
+    }
+    return { pageStart, pageEnd }
+  };
+
+
+  const buttons = document.querySelector('.buttons');
+  buttons.children[0].addEventListener('click',(event)=>{
+    if(page > 1){
+      page = page - 1;
+    }
+    const {pageStart, pageEnd} = getPageStartEnd(limit,page)
+    render(ul, pageStart, pageEnd);
+  })
+
+  buttons.children[1].addEventListener('click',()=>{
+    if((limit * page) < data.length - 1){
+      page = page + 1;
+    }
+    const {pageStart, pageEnd} = getPageStartEnd(limit, page)
+    render(ul, pageStart, pageEnd);
+  })
+  // reset 로컬스토리지
+  // buttons.children[2].addEventListener('click', ()=>{
+  //   localStorage.removeItem('agoraStatesDiscussions');
+  //   data = agoraStatesDiscussions.slice();
+  //   limit = 10; page = 1;
+  //   render(ul, 0, limit)
+  // })
+// // 디스커션 추가 기능 구현, `section.form__container` 요소에 새로운 아고라 스테이츠 질문을 추가할 수 있는 입력 폼을 제작
+// //`agoraStatesDiscussions` 배열에 추가한 데이터가 실제 쌓여야 합니다.
+
+// 문서 내용 확인
 const form = document.querySelector("form.form");
-const title = document.querySelector("div.form__input--title > input");
-const nameInput = document.querySelector("div.form__input--name > input");
-const textbox = document.querySelector("div.form__textbox > textarea");
+const author = form.querySelector("div.form__input--name > input");
+const title = form.querySelector("div.form__input--title > input");
+const textbox = form.querySelector("div.form__textbox > textarea");
 
+// 문서 내용 제출
 form.addEventListener("submit", (event) => { 
-  event.preventDefault(); //넣어야하는 이유? 지우면 생겼다 다시 사라짐.. submit 됨과 동시에 창이 다시 실행되기 때문에 다시 초기 화면으로 돌아오게 됨. preventDefault로 방지할 수 있음.
+  event.preventDefault(); //넣어야하는 이유? 지우면 생겼다 다시 사라짐.. submit 됨과 동시에 창이 새로고침 되기 때문에 다시 초기 화면으로 돌아오게 됨. preventDefault로 방지할 수 있음.
 
   //새로운 객체를 만들어야 한다.
   //input에 입력된 값(value)를 넣은 새로운 객체.
@@ -63,43 +126,21 @@ form.addEventListener("submit", (event) => {
   //더미 데이터(agoraStatesDiscussions)에도 추가해준다.(배열안에 앞으로 추가)  
   const obj = {
       id: "unique id",
-      createdAt: new Date(), //event가 발생한 시각이 기록됨
+      createdAt: new Date().toISOString, //event가 발생한 시각이 기록됨
       title: title.value,
       url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
-      author: nameInput.value,
+      author: author.value,
       answer: null,
       bodyHTML: textbox.value,
       avatarUrl:
         "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
     };
-  agoraStatesDiscussions.unshift(obj); //더미데이터의 맨 앞에 추가
-  const newDiscussion = convertToDiscussion(obj); //convertToDiscussion함수에 생성한 객체를 전달해서
-  ul.prepend(newDiscussion); //ul요소의 맨 앞에 추가
+  data.unshift(obj); //더미데이터의 맨 앞에 추가
 
+  // 로컬 스토리지에 저장
+
+  localStorage.setItem('agoraStatesDiscussions', JSON.stringify(data))
+
+  // 렌더링
+  render(ul, 0 , limit);
 })
-
-
-
-//<advanced>
-
-//페이지네이션 기능: 한 페이지에 10개씩 디스커션이 보여야 합니다. 
-//다음 페이지로 넘어갈 수 있어야 합니다.이전 페이지로 돌아올 수 있어야 합니다.
-//다음 페이지가 없거나, 이전 페이지가 없는 경우 페이지를 유지해야 합니다.
-
-
-//디스커션 유지 기능: LocalStorage에 대해서 스스로 학습하고, 새롭게 추가하는 Discussion이 페이지를 새로고침해도 유지되도록 제작합니다.
-
-
-// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
-const render = (element) => {
-  //element에 들어오는 것: ul 요소
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
-    //데이터를 DOM으로 바꿔준 것을 반복적으로 append
-  }
-  return;
-};
-
-// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
-render(ul);
